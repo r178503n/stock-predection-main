0c075efdd8bb07d51ffcbcd3c7b4543b54e260af
Kumbirai Bodzo <bodzok@gmail.com>
2021-07-04 18:57:41 +0200

    cleaned files

--- WebApp/Intelligence/serializers/video_serializer copy.py
+++
@@ -1,8 +0,0 @@
-
-import pandas as pd
-from Intelligence.models import Prediction, Video
-from rest_framework import serializers
-class VideoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Video
-        fields = ['file']
