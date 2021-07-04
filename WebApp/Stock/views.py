c7290e6cede2a2dff5eb7897bd5e6cfaba5c9eda
Kumbirai Bodzo <bodzok@gmail.com>
2021-07-04 15:15:44 +0200

    updated

--- WebApp/Stock/views.py
+++ WebApp/Stock/views.py
@@ -9,21 +9,25 @@ from .stock_app import *
 
 # Create your views here.
 
-stocks = [
+stock_context = dict({'stock_list': [
           {'key':'NGM', 'name':'NSE-TATAGLOBAL'},
           {'key':'MSFT','name':'Microsoft'},
             {'key':'NMS','name':'Facebook'}
-]
+]})
 
 def index(request):
-    context = dict({'fdsf':'fds'})
-    list = ['NSE-TATAGLOBAL','two','three']
-    context.setdefault('stock_list',stocks)
-    return render(request,'index.html', context)
+    #context = dict({'fdsf':'fds'})
+    #list = ['NSE-TATAGLOBAL','two','three']
+    #context.setdefault('stock_list',stocks)
+
+    stock_context.setdefault('status', None)
+
+    return render(request,'index.html', stock_context)
 
 def predictionView(request,name):
     import yfinance as yf
     data = yf.download(tickers=name, period='6d', interval='1m')
+    
 
     print(data)
     print(name)
@@ -42,20 +46,26 @@ def predictionView(request,name):
     return render(request,'predicted.html', context)
 
 def trainView(request, name):
-    import yfinance as yf
-    data = yf.download(tickers=name, period='5d', interval='5m')
-    #settings.MEDIA_ROOT+'/NSE-TATAGLOBAL.csv
 
-    training = TrainModel(data=data, train_name=name+'_trained_model')
-    training.clean()
-    training.train()
+    try:
+        import yfinance as yf
+        data = yf.download(tickers=name, period='5d', interval='5m')
+        #settings.MEDIA_ROOT+'/NSE-TATAGLOBAL.csv
 
-    context = dict()
-    print(name)
-    
-    context.setdefault('name',name)
+        training = TrainModel(data=data, train_name=name+'_trained_model')
+        training.clean()
+        training.train()
+
+        # context = dict()
+        print(name)
+        
+        stock_context.setdefault('status',{'code':1,'message':'successfully trained for '+name})
+
+    except:
+        stock_context.setdefault('status',{'code':0,'message':'training failed for'+name})
+
     # get_dashboard(stock_name = name)
     
     # return render(request,'index.html', context)
-    return index(request)
+    return render(request,'index.html', stock_context)
 
